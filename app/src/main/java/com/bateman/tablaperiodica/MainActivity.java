5e414a2efd345a01d0710ffb7dc24b576d9cd1f2
alejandro <alexlobo.97@gmail.com>
2021-07-19 02:37:53 -0500

    5 sounds

--- app/src/main/java/com/bateman/tablaperiodica/MainActivity.java
+++ app/src/main/java/com/bateman/tablaperiodica/MainActivity.java
@@ -9,12 +9,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 
     ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter asniAdapter;
+    MediaPlayer mediaPlayer;
     private Button buttonAudio;
 
     @Override
@@ -23,28 +27,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView=(ListView)findViewById(R.id.listview);
+        arrayList = new ArrayList<String>();
+        Field[] fields = R.raw.class.getFields();
 
-        ArrayList<String> arrayList=new ArrayList<>();
+        for (int i =0;i<fields.length;i++){
+            arrayList.add(fields[i].getName());
+        }
 
-        arrayList.add("Oxigeno 1");
-        arrayList.add("Oxigeno 2");
-        arrayList.add("Oxigeno 3");
-        arrayList.add("Oxigeno 4");
+        asniAdapter= new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(asniAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(mediaPlayer != null){
+                    mediaPlayer.release();
+                }
+
+                int resId = getResources().getIdentifier(arrayList.get(i),"raw",getPackageName());
+                mediaPlayer = MediaPlayer.create(MainActivity.this,resId);
+                mediaPlayer.start();
+            }
+        });
+
+        /*ArrayList<String> arrayList=new ArrayList<>();
+
+        arrayList.add("Actinio");
+        arrayList.add("Aluminio");
+        arrayList.add("Americio");
+        arrayList.add("Antimonio");
+        arrayList.add("Argon");
 
         ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
 
         final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.notification_sound_effect);
         final MediaPlayer mediaPlayer2 = MediaPlayer.create(this,R.raw.notification_sound_effect2);
 
+        final MediaPlayer tp_1 = MediaPlayer.create(this,R.raw.actinio);
+        final MediaPlayer tp_2 = MediaPlayer.create(this,R.raw.aluminio);
+        final MediaPlayer tp_3 = MediaPlayer.create(this,R.raw.americio);
+        final MediaPlayer tp_4 = MediaPlayer.create(this,R.raw.antimonio);
+        final MediaPlayer tp_5 = MediaPlayer.create(this,R.raw.argon);
+
         listView.setAdapter((arrayAdapter));
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 if (i == 0){
-                    mediaPlayer.start();
+                    tp_1.start();
+                    tp_1.stop();
+                }
+                if (i==1){
+                    tp_2.start();
                 }
                 if (i==2){
-                    mediaPlayer2.start();
+                    tp_3.start();
+                }
+                if (i==3){
+                    tp_4.start();
+                }
+                if (i==4){
+                    tp_5.start();
+                }
+            }
+
+            private void stopPlaying() {
+                if (tp_1 != null) {
+                    tp_1.stop();
+                    tp_1.release();
+                }
+                if (tp_2 != null) {
+                    tp_2.stop();
+                    tp_2.release();
                 }
             }
         });
@@ -55,6 +108,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 mediaPlayer.start();
             }
-        });
+        });*/
     }
 }
