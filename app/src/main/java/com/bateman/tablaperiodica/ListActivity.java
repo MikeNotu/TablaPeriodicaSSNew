65c48773d768f853d749aeebbe3bef5f1271f7c8
alejandro <alexlobo.97@gmail.com>
2021-11-10 00:51:44 -0600

    buttons

--- app/src/main/java/com/bateman/tablaperiodica/ListActivity.java
+++ app/src/main/java/com/bateman/tablaperiodica/ListActivity.java
@@ -1,5 +1,6 @@
 package com.bateman.tablaperiodica;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import androidx.appcompat.app.AppCompatActivity;
 //import android.support.v7.app.AppCompatActivity;
@@ -50,6 +51,27 @@ public class ListActivity extends AppCompatActivity {
                 int resId = getResources().getIdentifier(arrayList.get(i),"raw",getPackageName());
                 mediaPlayer = MediaPlayer.create(ListActivity.this,resId);
                 mediaPlayer.start();
+            }
+        });
+
+        Button b1 = (Button) findViewById(R.id.btn_menu);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+            }
+        });
+        Button b2 = (Button) findViewById(R.id.btn_tabla);
+        b2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), TableActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                overridePendingTransition(0,0);
             }
         });
 
--- app/src/main/java/com/bateman/tablaperiodica/TableActivity.java
+++ app/src/main/java/com/bateman/tablaperiodica/TableActivity.java
@@ -1,5 +1,6 @@
 package com.bateman.tablaperiodica;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import androidx.appcompat.app.AppCompatActivity;
 //import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,25 @@ public class TableActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
 
-
+        Button b1 = (Button) findViewById(R.id.btn_menu2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+            }
+        });
+        Button b2 = (Button) findViewById(R.id.btn_lista);
+        b2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ListActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+            }
+        });
     }
 }
